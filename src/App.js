77b49eb276488cc1a7ed59095c40cd30d34abe6a
Jacob Westman <hello@jacobwestman.com>
2018-02-08 23:54:26 +0100

    Added bootstrap and react-bootstrap to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
+import { Grid, Navbar } from 'react-bootstrap';
+import teaserImage from './ontopofthemountain.jpeg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">Home</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to jwcom playground</h1>
+          <img src={teaserImage} alt="teaserImage" width="100%" />
         </header>
         <section />
       </div>
